6b6bb17e2a318002bb0ef62d0f0589dad538f900
NanoCat <shablya24@gmail.com>
2016-04-19 13:45:24 +0300

    Mark 'final'

--- files/vis/src/java/Vis.java
+++ files/vis/src/java/Vis.java
@@ -6,7 +6,8 @@ import javax.sound.sampled.AudioInputStream;
 
 // TODO: Replace with something better
 public class Vis {
-    public static double[][] getWaveForm(AudioInputStream audioStream) throws IOException {
+    public static double[][] getWaveForm(final AudioInputStream audioStream)
+            throws IOException {
         // TODO?: Make it work with any encoding
         if (audioStream.getFormat().getEncoding() != Encoding.PCM_UNSIGNED) {
             throw new IOException("Bad sample rate");
@@ -22,18 +23,18 @@ public class Vis {
             throw new IOException("Bad sample rate");
         }
 
-        long frameLength = audioStream.getFrameLength();
-        long frameSize   = audioStream.getFormat().getFrameSize();
+        final long frameLength = audioStream.getFrameLength();
+        final long frameSize   = audioStream.getFormat().getFrameSize();
 
-        byte[] rawData = new byte[(int)(frameLength * frameSize)];
+        final byte[] rawData = new byte[(int)(frameLength * frameSize)];
         audioStream.read(rawData);
 
         // FIXME: This only works with 8 bit samples
-        double[][] peaks = new double[1][(int)frameLength];
+        final double[][] peaks = new double[1][(int)frameLength];
         int peak = 0;
 
         for (int frame = 0; frame < (int)frameLength; frame += 1) {
-            peaks[0][peak] = rawData[frame];
+            peaks[0][peak] = (double)rawData[frame] / 127.0;
             peak += 1;
         }
 
