38a40362f0e05551dff7a06e58bac7257bab9a45
NanoCat <shablya24@gmail.com>
2016-04-19 13:46:03 +0300

    Graph plotter

--- files/vis/src/java/Main.java
+++ files/vis/src/java/Main.java
@@ -2,47 +2,60 @@ package vis;
 
 import javafx.application.Application;
 import java.io.File;
-import javafx.scene.Scene;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import javax.imageio.ImageIO;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 
 public class Main extends Application {
+    private BufferedImage plotWaveGraph(final double[][] peaks) {
+        final BufferedImage graph = new BufferedImage(peaks[0].length, 256,
+                BufferedImage.TYPE_BYTE_INDEXED);
+
+        final Graphics2D gfx = graph.createGraphics();
+
+        gfx.setColor(Color.GREEN);
+        int lastY = 0;
+        for (int i = 1; i < peaks[0].length; i += 1) {
+            // Each peak ranges from -1.0 to 1.0, upscale it to buffer size and
+            // offset a bit from edges of the buffer.
+            final int Y = (int)(peaks[0][i] * graph.getHeight() / 2.2)
+                    + (graph.getHeight() / 2);
+            gfx.drawLine(i - 1, lastY, i, Y);
+            lastY = Y;
+        }
+
+        gfx.dispose();
+
+        return graph;
+    }
+
     @Override
-    public void start(Stage stage) throws Exception {
-        FileChooser fileChooser = new FileChooser();
+    public void start(final Stage stage) throws Exception {
+        final FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().add(
                 new ExtensionFilter("All files", "*.*"));
 
-        File audioFile = fileChooser.showOpenDialog(stage);
+        final File audioFile = fileChooser.showOpenDialog(stage);
         if (audioFile == null) {
             return;
         }
 
-        AudioInputStream audioStream =
+        final AudioInputStream audioStream =
                 AudioSystem.getAudioInputStream(audioFile);
 
-        double[][] peaks = Vis.getWaveForm(audioStream);
+        final double[][] peaks = Vis.getWaveForm(audioStream);
 
-        final XYChart.Series series = new XYChart.Series();
+        final BufferedImage waveGraph = plotWaveGraph(peaks);
 
-        for (int i = 0; i < peaks[0].length; i += 512) {
-            series.getData().add(new XYChart.Data(i, peaks[0][i]));
-        }
-
-        // TODO: Find a better way to render this
-        final LineChart<Number, Number> waveChart =
-                new LineChart<>(new NumberAxis(), new NumberAxis());
-        waveChart.getData().add(series);
-
-        final Scene scene = new Scene(waveChart, 640, 480);
-        stage.setScene(scene);
+        // TODO: REMOVEME
+        ImageIO.write(waveGraph, "png", new File("graph.png"));
 
         stage.setTitle("vis");
         stage.show();
