cac89ee60ba86506bf884e9d6f1c600555496b9c
NanoCat <shablya24@gmail.com>
2016-04-23 16:36:13 +0300

    Fix wave graph being inverted

--- files/vis/src/java/Main.java
+++ files/vis/src/java/Main.java
@@ -4,9 +4,11 @@ import javafx.application.Application;
 
 import java.awt.*;
 import java.io.File;
+
 import javafx.stage.Stage;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.imageio.ImageIO;
@@ -14,8 +16,8 @@ import java.awt.image.BufferedImage;
 
 
 public class Main extends Application {
-    private BufferedImage plotWaveGraph(final double[][] peaks,
-                                        Viewport view) {
+    private BufferedImage plotGraph(final double[] peaks,
+                                    Viewport view) {
         // TODO: Test with multichannel audio, various sample rates, LE, BE
 
         final BufferedImage graph = new BufferedImage(
@@ -34,28 +36,36 @@ public class Main extends Application {
         final int h = graph.getHeight();
         final int precision = Math.max((view.getX1() - view.getX0()) / w, 1);
 
-        for (int chan = 0; chan < peaks.length; chan += 1) {
-            int prevX = 0;
-            int prevY = h / 2;
-            for (int i = view.getX0(); i < view.getX1(); i += precision) {
-                // Scale x and y to fit buffer
-                final int x = (int) (((double) (i - view.getX0())
-                        / (double) (view.getX1() - view.getX0())) * (double) w);
-                final int y = (h / 2) + (int) (peaks[chan][i]
-                        * (double) h / (2.0 * view.getPadding()));
+        int prevX = 0;
+        int prevY = h / 2;
+        for (int i = Math.max(view.getX0(), 0);
+             i < Math.min(view.getX1(), peaks.length);
+             i += precision) {
+            // Scale x and y to fit buffer
+            final int x = (int) (((double) (i - view.getX0())
+                    / (double) (view.getX1() - view.getX0())) * (double) w);
+            final int y = (h / 2) - (int) (peaks[i]
+                    * (double) h / (2.0 * view.getPadding()));
 
-                // First vertex has no links going before it
-                if (i > view.getX0()) {
-                    gfx.drawLine(prevX, prevY, x, y);
-                }
-
-                prevX = x;
-                prevY = y;
+            // First vertex has no links going before it
+            if (i > view.getX0()) {
+                gfx.drawLine(prevX, prevY, x, y);
             }
+
+            prevX = x;
+            prevY = y;
         }
 
         gfx.dispose();
         return graph;
+    }
+
+    private BufferedImage plotSpectromgram(final double[] peaks, int interval,
+                                           Viewport view) {
+        return new BufferedImage(
+                view.getTargetW(),
+                view.getTargetH(),
+                BufferedImage.TYPE_BYTE_INDEXED);
     }
 
     @Override
@@ -74,11 +84,15 @@ public class Main extends Application {
 
         final double[][] peaks = Vis.getWaveForm(audioStream);
 
-        final BufferedImage waveGraph = plotWaveGraph(peaks,
+        final BufferedImage graph = plotGraph(peaks[0],
                 new Viewport(0, peaks[0].length, 1024, 256, 1.5));
 
+        final BufferedImage spectrogram = plotSpectromgram(peaks[0], 30,
+                new Viewport(0, 0, peaks[0].length, 22000, 1024, 256, 1.0));
+
         // TODO: REMOVEME
-        ImageIO.write(waveGraph, "png", new File("graph.png"));
+        ImageIO.write(graph, "png", new File("graph.png"));
+        ImageIO.write(spectrogram, "png", new File("spectrogram.png"));
 
         stage.setTitle("vis");
         stage.show();
