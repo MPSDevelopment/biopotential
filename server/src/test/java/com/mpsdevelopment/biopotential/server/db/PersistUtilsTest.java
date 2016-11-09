e7c54b0f43d206a43c266fa0ec3b3170e0350b8d
Aurusd <rebrov@bigmir.net>
2016-11-09 17:23:14 +0200

    - update some test's

--- server/src/test/java/com/mpsdevelopment/biopotential/server/db/PersistUtilsTest.java
+++ server/src/test/java/com/mpsdevelopment/biopotential/server/db/PersistUtilsTest.java
@@ -3,7 +3,7 @@ package com.mpsdevelopment.biopotential.server.db;
 import com.mpsdevelopment.biopotential.server.db.dao.FoldersDao;
 import com.mpsdevelopment.biopotential.server.db.dao.UserDao;
 import com.mpsdevelopment.biopotential.server.db.pojo.Folder;
-import com.mpsdevelopment.biopotential.server.db.pojo.Pattern;
+import com.mpsdevelopment.biopotential.server.db.pojo.User;
 import com.mpsdevelopment.biopotential.server.settings.ServerSettings;
 import com.mpsdevelopment.plasticine.commons.logging.Logger;
 import com.mpsdevelopment.plasticine.commons.logging.LoggerUtil;
@@ -13,7 +13,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.criterion.Restrictions;
+import org.hibernate.service.ServiceRegistry;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -41,6 +41,8 @@ public class PersistUtilsTest {
 
     @Autowired
     private FoldersDao foldersDao;
+
+    private static ServiceRegistry serviceRegistry;
 
 
     public PersistUtilsTest() {
@@ -71,17 +73,38 @@ public class PersistUtilsTest {
     @Test
     public void changeConfigureSessionFactoryTest() throws HibernateException {
         SessionFactory sessionFactory = persistUtils.configureSessionFactory();
-        Criteria queryNewDb = sessionFactory.openSession().createCriteria(Folder.class).setCacheable(false);
+        Session session = sessionFactory.openSession();
+        Criteria queryNewDb = session.createCriteria(Folder.class).setCacheable(false);
         List<Folder> foldersFromNewDb = queryNewDb.list();
+        Assert.assertEquals(14,foldersFromNewDb.size());
 
-        Configuration configuration = new Configuration();
-        configuration.setProperty("hibernate.connection.url", "jdbc:h2:file:./data/databaseArk" /*+ serverSettings.getDbPath()*/);
+        LOGGER.info("folders size %s", foldersFromNewDb.size());
+        sessionFactory.close();
+
+        Configuration configuration = new Configuration().addResource("hibernate.cfg.xml");
+        configuration.setProperty("hibernate.connection.url", "jdbc:h2:file:./data/databaseArk;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;MVCC=true;MODE=ORACLE;AUTO_SERVER=TRUE;INIT=CREATE SCHEMA IF NOT EXISTS main");
         configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
         configuration.setProperty("hibernate.connection.driver_class", "org.h2.Driver");
+        configuration.setProperty("hibernate.connection.password", "MiumVa");
+        configuration.setProperty("hibernate.connection.username", "root");
 
-        sessionFactory = configuration.buildSessionFactory();
-        Criteria query = sessionFactory.openSession().createCriteria(Folder.class).setCacheable(false);
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure().loadProperties("hibernateArk.properties").build();
+        sessionFactory = new Configuration().buildSessionFactory(serviceRegistry);
+
+//        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySetting("hibernate.cfg.xml",configuration.getProperties()).build();
+//        sessionFactory = configuration.buildSessionFactory();
+        LOGGER.info("Connection changed to %s", configuration.getProperty("hibernate.connection.url"));
+
+        session = sessionFactory.openSession();
+
+        Criteria query = session.createCriteria(Folder.class).setCacheable(false);
         List<Folder> folders = query.list();
+        LOGGER.info("folders size %s", folders.size());
+        Assert.assertEquals(5,folders.size());
+
+        Criteria queryUsers = sessionFactory.openSession().createCriteria(User.class).setCacheable(false);
+        List<User> users = queryUsers.list();
+        LOGGER.info("users size %s", users.size());
 
         sessionFactory.close();
         Assert.assertTrue(sessionFactory.isClosed());
