6e03b910353bf14b9fb4c2068cb51e676eae4503
Aurusd <rebrov@bigmir.net>
2016-12-08 16:36:53 +0200

    - add ConverterApplication

--- server/src/main/java/com/mpsdevelopment/biopotential/server/gui/ConverterApplication.java
+++ server/src/main/java/com/mpsdevelopment/biopotential/server/gui/ConverterApplication.java
@@ -1,5 +1,6 @@
 package com.mpsdevelopment.biopotential.server.gui;
 
+import com.mpsdevelopment.biopotential.server.JettyServer;
 import com.mpsdevelopment.biopotential.server.gui.converter.ConverterPanel;
 import com.mpsdevelopment.biopotential.server.settings.StageSettings;
 import com.mpsdevelopment.biopotential.server.utils.StageUtils;
@@ -9,13 +10,14 @@ import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 
 public class ConverterApplication extends Application{
 
     private static final Logger LOGGER = LoggerUtil.getLogger(BioApplication.class);
-    public static XmlWebApplicationContext WEB_CONTEXT;
-    public static final String SPRING_CONTEXT_FILENAME = "web-context.xml";
+    public static final AbstractApplicationContext APP_CONTEXT = new ClassPathXmlApplicationContext("webapp/web-context.xml");
 
     public static void main(String[] args) {
 
@@ -30,10 +32,14 @@ public class ConverterApplication extends Application{
 
     }
 
+    @Override
+    public void stop() {
+
+        System.exit(0);
+    }
+
     private void addMainPanel() {
-        WEB_CONTEXT = new XmlWebApplicationContext();
-        WEB_CONTEXT.setConfigLocations(SPRING_CONTEXT_FILENAME);
-        WEB_CONTEXT.setParent(BioApplication.APP_CONTEXT);
+
         ConverterPanel converterPanel = new ConverterPanel();
         LOGGER.info(" Start Converter application");
         Stage mainPanelStage = StageUtils.createStage(null, converterPanel, new StageSettings().setPanelTitle("Конвертор базы").setClazz(converterPanel.getClass()).setHeight(260d).setWidth(370d).setHeightPanel(250d).setWidthPanel(370d)/*.setX(StageUtils.getCenterX()).setY(StageUtils.getCenterY())*/);
