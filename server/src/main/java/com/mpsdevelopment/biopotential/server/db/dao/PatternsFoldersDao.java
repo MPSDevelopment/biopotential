d28e01c013af1f22783314017e0cb4acbac0e2eb
dmitriyyavorskiy <dmitriy.yavorskiy@gmail.com>
2016-12-14 15:52:10 +0200

    Small getByPattern refactoring

--- server/src/main/java/com/mpsdevelopment/biopotential/server/db/dao/PatternsFoldersDao.java
+++ server/src/main/java/com/mpsdevelopment/biopotential/server/db/dao/PatternsFoldersDao.java
@@ -5,13 +5,15 @@ import com.mpsdevelopment.biopotential.server.db.pojo.PatternsFolders;
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
 
-public class PatternsFoldersDao extends GenericDao<PatternsFolders,Long> {
+public class PatternsFoldersDao extends GenericDao<PatternsFolders, Long> {
 
-    public PatternsFolders getByPattern(Pattern pattern) {
-        Criteria query = getSession().createCriteria(PatternsFolders.class).setCacheable(false);
-        query.add(Restrictions.eq(PatternsFolders.PATTERNS, pattern));
-        PatternsFolders patternsFolders = (PatternsFolders) query.uniqueResult();
-        if (!(patternsFolders == null)) {return patternsFolders;}
-        else return new PatternsFolders();
-    }
+	public PatternsFolders getByPattern(Pattern pattern) {
+		Criteria query = getSession().createCriteria(PatternsFolders.class).setCacheable(false);
+		query.add(Restrictions.eq(PatternsFolders.PATTERNS, pattern));
+		PatternsFolders patternsFolders = (PatternsFolders) query.uniqueResult();
+		if (patternsFolders != null) {
+			return patternsFolders;
+		}
+		return new PatternsFolders();
+	}
 }
